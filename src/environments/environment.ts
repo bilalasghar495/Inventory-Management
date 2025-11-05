249acf9555ca18f1c0c9ba3d9e9768e775e8485b
Bilal Asghar <bilalasghar586@gmail.com>
2025-11-05 17:42:25 +0500

    update the api Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://e84312bc3894.ngrok-free.app',
+  apiUrl: 'https://inventory-management-qieh.onrender.com',
   socketUrl: 'wss://inventory-management-qieh.onrender.com'
 };
