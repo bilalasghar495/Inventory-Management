9ae96396dcca5cf18f7f4ab72f57f8a1d7a20687
Bilal Asghar <bilalasghar586@gmail.com>
2025-11-06 15:33:41 +0500

    Minor Fixing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://e84312bc3894.ngrok-free.app',
-  socketUrl: 'wss://1a3a2c3bccee.ngrok-free.app'
+  apiUrl: 'https://inventory-management-qieh.onrender.com',
+  socketUrl: 'wss://inventory-management-qieh.onrender.com'
 };
